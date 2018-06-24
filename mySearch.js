02f7ff1f5e9aad328279f4c0cc3d909ebf81ed41
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-06-24 03:55:07 +0300

    Create mySearch.js

---
+++ mySearch.js
@@ -0,0 +1,11 @@
+var cx = '006471411703522520933:xii2ksrstcc';
+var gcse = document.createElement('script');
+gcse.type = 'text/javascript';
+gcse.async = true;
+gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+var s = document.getElementsByTagName('script')[0];
+s.parentNode.insertBefore(gcse, s);
+
+document.getElementById("everythingIsSet").addEventListener("click",function () {
+alert("We are working on it  currently please hold");
+});
