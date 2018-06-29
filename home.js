0f1dad2d5100aa63f32f29a4ac6d218febc703c5
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-06-30 02:45:08 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,27 +1,31 @@
 $("document").ready(function(){
 $("#vid2").hide();
 $("#vid1").show();
+$("#map_disp").hide();
 });
 
 
 $("#displayvid1").click(function(){
 $("#vid2").hide();
 $("#vid1").show();
+$("#map_disp").hide();
 });
 $("#displayvid2").click(function(){
 $("#vid1").hide();
 $("#vid2").show();
+$("#map_disp").hide();
 });
 
 
 $("#map").click(function(){
 $("#vid2").hide();
 $("#vid1").hide();
+$("#map_disp").show();
 function myMap() {
 var mapProp= {
     center:new google.maps.LatLng(51.508742,-0.120850),
     zoom:5,
 };
-var map=new google.maps.Map(document.getElementById("vid1"),mapProp);
+var map=new google.maps.Map(document.getElementById("map_disp"),mapProp);
 }
 });
