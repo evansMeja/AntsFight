a626a8a12bc202a4e2ea08e84c9330eadae98e9c
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-06-30 00:55:43 +0300

    Update home.js

--- home.js
+++ home.js
@@ -1,3 +1,14 @@
 $("#displayvid1").click(function(){
 $("#vid1").css("visibility","visible");
 });
+
+
+$("#map").click(function(){
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("displayvid1"),mapProp);
+}
+});
