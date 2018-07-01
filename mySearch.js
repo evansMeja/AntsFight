98e4eca9ef0a13138bf2d97fd3b556eedd03d1cf
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-07-01 14:45:32 +0300

    Update mySearch.js

--- mySearch.js
+++ mySearch.js
@@ -1,36 +1,7 @@
-var cx = '006471411703522520933:xii2ksrstcc';
-var gcse = document.createElement('script');
-gcse.type = 'text/javascript';
-gcse.async = true;
-gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-var s = document.getElementsByTagName('script')[0];
-s.parentNode.insertBefore(gcse, s);
-
-document.getElementById("everythingIsSet").addEventListener("click",function () {
-alert("We are working on it  currently please hold");
- sendMail();
-});
-
-function sendMail(){
- 
-alert("Now in mail");
-  $.ajax({
-    type: "POST",
-    url: "https://mandrillapp.com/api/1.0/messages/send.json",
-    data: {
-      'key': 'baf944f4342495b15705b39f27c0da26-us18',
-      'message': {
-        'from_email': 'evanslagat910@gmail.com',
-        'to': [
-          {
-            'email': 'evanslagat911@gmail.com',
-            'name': 'Tricky Programmer',
-            'type': 'to'
-          }
-        ],
-        'subject': 'title',
-        'html': 'html can be used'
-      }
-    }
-  });
+function myMap() {
+var mapProp= {
+    center:new google.maps.LatLng(51.508742,-0.120850),
+    zoom:5,
+};
+var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
 }
