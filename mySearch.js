cc97bc1dd64cfbe8436ea4d77e88fbf5a8d7d551
Kiplagat Evans Kiprotich <35095785+evansMeja@users.noreply.github.com>
2018-07-01 14:48:32 +0300

    Update mySearch.js

--- mySearch.js
+++ mySearch.js
@@ -1,7 +1,12 @@
+document.getElementById("map").addEventListener(click,function(){
+alert("clicked me");
+myMap();
+});
+
 function myMap() {
 var mapProp= {
     center:new google.maps.LatLng(51.508742,-0.120850),
     zoom:5,
 };
-var map=new google.maps.Map(document.getElementById("googleMap"),mapProp);
+var map=new google.maps.Map(document.getElementById("map_disp"),mapProp);
 }
