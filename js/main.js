0693129a6a9a7dc95226156ca980f0a4b84a722d
evanslee <35095785+evansMeja@users.noreply.github.com>
2018-08-05 19:50:15 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 $(document).ready(function(){
 	
+$( "#tabs" ).tabs();
+	
 $("#uploadAnyFile").click(function(){
 var fileName=$("#fileName").val();
 var fileDescription=$("#fileDescription").val();
