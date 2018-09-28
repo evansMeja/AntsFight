7b6f64e805fb0fa7e51a149ee1a30b09049dd471
evanslee <35095785+evansMeja@users.noreply.github.com>
2018-09-28 01:43:34 -0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,171 +1,89 @@
-$(document).ready(function(){
-$(".search1").focus(function(){
-$("#machineLearningSearchEngine").modal();
-});
-	
-(function(){
-var id;
-var msg;
-document.getElementById("submitSite").addEventListener("click",function(){
-bypassBlockedSites();
-});
+//functions executed immediately
+setSearchBarVariable();
+//setIdEventListener(elementLocation ,actionListener, payload);
+setIdEventListener("submitSite" ,"click", bypassThisBlockedSite(acessValue("")));
 
+//JQUERY DEPENDENT CODE IS HERE
+//functions excecuted after the page loads
+$(document).read(function(){
+setIdEventListener("search1" ,"mousedown", showThisModal("machineLearningSearchEngine"));
+setIdEventListener("demo" ,"click", submitThisDetailsFromEmailClient(["email","name","comment","website"]));
+})();
+
+function setSearchBarVariable(){
+	var cx = '006471411703522520933:xii2ksrstcc';
+	var gcse = document.createElement('script');
+	gcse.type = 'text/javascript';
+	gcse.async = true;
+	gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
+	var s = document.getElementsByTagName('script')[0];
+	s.parentNode.insertBefore(gcse, s);
+	setThisHTML('gsc-i-id1',"Placeholder",'search anywhere');
+}  
+
+//<script src="https://cse.google.com/cse.js?cx=006471411703522520933:xii2ksrstcc" type="text/javascript"></script>
+	
+//GLOBAL FUNCTIONS
 function bypassBlockedSites(){
-var starturl = "http://www.google.com/translate?langpair=ja|en&u=";
-var sitename = acessHTML("sitename");
-var fullurl = "www."+sitename+".com" 
-var hackurl = starturl+fullurl;
-id = "results";
-msg = "ok";
-confirmAcess(hackurl,fullurl);
+	var id;
+	var msg;
+	var starturl = "http://www.google.com/translate?langpair=ja|en&u=";
+	var sitename = acessHTML(arguments[0]);
+	var fullurl = "www."+sitename+".com" 
+	var hackurl = starturl+fullurl;
+	id = "results";
+	msg = "ok";
+	if(confirm("are you sure "+fullurl+" is blocked by your ISP?")){
+		var win = window.open(hackurl, '_blank');
+		win.focus();
+	}
 }
 
-function confirmAcess(hackurl,fullurl){
-if(confirm("are you sure "+fullurl+" is blocked by your ISP?")){
-alert("Thanks");
-openInNewTab(hackurl);
-}
-}
-
-function openInNewTab(url) {
-  var win = window.open(url, '_blank');
-  win.focus();
-}
-
-
-
-function acessHTML(id){
-return document.getElementById(id).value;
-}
-
-function acessValue(id){
-return document.getElementById(id).value;
-}
-
-function setHTML(id,msg){
-return document.getElementById(id).innerHTML = msg;
-}
-
-function setValue(id){
-return document.getElementById(id).value;
-}
-})();	
-	
-  (function() {
-    var cx = '006471411703522520933:xii2ksrstcc';
-    var gcse = document.createElement('script');
-    gcse.type = 'text/javascript';
-    gcse.async = true;
-    gcse.src = 'https://cse.google.com/cse.js?cx=' + cx;
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(gcse, s);
-  })();
-
-document.getElementById('gsc-i-id1').placeholder = 'type here to search for programming resources..';
-
-$(".tab-content").tabs();
-	
-$("#uploadAnyFile").click(function(){
-var fileName=$("#fileName").val();
-var fileDescription=$("#fileDescription").val();
-$.ajax({
-    url: "https://formspree.io/evanslagat911@gmail.com",
-    method: "POST",
-    data: {fileName: fileName,fileDescription:fileDescription},
-    dataType: "json",
-    success: function(response){
-            if(response.status == "success"){
-                alert("We received your submission, thank you!");
-            }else{
-                alert("An error occured.");
-            }
-}
-}); 
+//modal master
+function showThisModal(){
+$("#"+arguments[0]).modal();
 });
 	
-$(".ajaxForm").submit(function(e){
-    e.preventDefault();
-    var href = $(this).attr("action");
-    $.ajax({
-        type: "POST",
-        url: href,
-        data: new FormData(this),
-        dataType: "json",
-        processData: false,
-        contentType: false,
-        success: function(response){
-            if(response.status == "success"){
-                alert("We received your submission, thank you!");
-            }else{
-                alert("An error occured.");
-            }
-        }
-    });
-});
-	
-$("#demo").click(function(){
-alert("I love programmig");
-var email=$("#email").val();
-var name=$("#name").val();
-var comment=$("#comment").val();
-var website=$("#website").val();
-$.ajax({
-    url: "https://formspree.io/evanslagat911@gmail.com",
-    method: "POST",
-    data: {email: email,name:name,comment:comment,website:website},
-    dataType: "json",
-    success: function(response){
-            if(response.status == "success"){
-                alert("We received your submission, thank you!");
-            }else{
-                alert("An error occured.");
-            }
+//set payload listeners
+function setIdEventListener(elementLocation ,actionListener, payload){
+document.getElementById(elementLocation).addEventListener(actionListener,payload);
 }
-}); 
+
+//function to send form data to developers email address
+function submitThisDetailsFromEmailClient(idFields){
+	$.ajax({
+		url: "https://formspree.io/evanslagat911@gmail.com",
+		method: "POST",
+		data:  getInnerHTMLValues(idFields),
+		dataType: "json",
+		success: function(response){
+		if(response.status == "success"){
+			alert(" submission sucessfull we will give you feedback as soon as possible");
+		}else{
+			alert(" submission failed please send error report to our beloved developers");
+		}
+		}
+	}); 
+     resetHTML(idFields);
+}		
 });
 
+//function to implement html content getters and setters
+var getThisHTML    = () =>  return document.getElementById(arguments[0]).arguments[1];
+var setThisHTML    = () =>  document.getElementById(arguments[0]).arguments[2] = arguments[3];
 
-	$("#portfolio-contant-active").mixItUp();
+//function to set the inner HTML of an elements to nothing
+function resetHTML(fields) {
+    for (var i=0; i < fields.length; i++) {
+	setThisHTML(fields[i],"innerHTML",""); 
+    }
+}
 
-
-	$("#testimonial-slider").owlCarousel({
-	    paginationSpeed : 500,      
-	    singleItem:true,
-	    autoPlay: 3000,
-	});
-
-
-
-
-	$("#clients-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-	$("#works-logo").owlCarousel({
-		autoPlay: 3000,
-		items : 5,
-		itemsDesktop : [1199,5],
-		itemsDesktopSmall : [979,5],
-	});
-
-
-	$("#tabs").tabs();
-	
-	
-	
-	// Counter
-
-	$('.counter').counterUp({
-        delay: 10,
-        time: 1000
-    });
-
-
-});
-
-
-
-
+//function to return the inner HTML of an elements in form of an array
+function getInnerHTMLValues(fields) {
+    valueBackPack=[];
+    for (var i=0; i < fields.length; i++) {
+	valueBackPack.push(getThisHTML(fields[i],'innerHTML')); 
+    }
+    return valueBackPack;
+}
